f3efe9038ae7cc4b4d36cd2d97282e9d449a36d6
yanina <yania.dabrowski@hotmail.com.ar>
2022-06-10 12:35:27 -0300

    se agrega react dom + nav bar link

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css';
 import Banner from './component/Banner/Banner';
 import Gallery from './component/Gallery/Gallery';
+import NavBar from './component/Navbar/Navbar';
+import About from './component/About/About';
+import Get from './component/Get/Get';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <BrowserRouter>
+      <NavBar/>
+      <Routes>
+     
+       <Route path='/Gallery' element={<Gallery />} />
+      <Route path='/About' element={<About />} />
+      <Route path='/Get' element={<Get />} />
+      </Routes>
+      </BrowserRouter>
       <Banner/>
-      <Gallery/>
+      
       </header>
     </div>
   );
---
+++ src/component/Navbar/Navbar.js
@@ -0,0 +1,24 @@
+import { NavLink } from 'react-router-dom'
+import './Navbar.css'
+
+
+
+const NavBar = () => {
+      return (
+          <nav>
+            <div className="filtros">
+              
+              <NavLink to='/' className={({ isActive }) => isActive ? 'ActiveOption' : 'Option'}>Home</NavLink>
+              <NavLink to='/Gallery' className={({ isActive }) => isActive ? 'ActiveOption' : 'Option'}>Gallery</NavLink>
+              <NavLink to='/About' className={({ isActive }) => isActive ? 'ActiveOption' : 'Option'}>About</NavLink>
+              <NavLink to='/Get' className={({ isActive }) => isActive ? 'ActiveOption' : 'Option'}>Get in touch!</NavLink>
+            
+           
+          </div>
+          
+              
+          </nav>
+      )
+  }
+  
+  export default NavBar
